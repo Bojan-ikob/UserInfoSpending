c96b4e35761d0733cccc3b3fce90756b2f62b062
petko <petkovski.bojan.8891@gmail.com>
2025-01-08 21:57:20 +0100

    obidi za povrzuvanje na tabeli

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 from extensions import db
 
 class UserInfo(db.Model):
-    __tablename__= "user_info"
-    user_id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(120), nullable = False)
-    email = db.Column(db.String(120), nullable = False)
-    age = db.Column(db.Integer, primary_key = True)
+    __tablename__ = "user_info"
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    age = db.Column(db.Integer, nullable=False)
+
     def to_dict(self):
-        return{
+        return {
             'user_id': self.user_id,
             'name': self.name,
             'email': self.email,
@@ -15,13 +16,14 @@ class UserInfo(db.Model):
         }
 
 class UserSpending(db.Model):
-    __tablename__= "user_spending"
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True)
-    money_spend = db.Column(db.Float, primary_key = True)
-    year = db.Column(db.Integer, primary_key = True)
+    __tablename__ = "user_spending"
+    user_id = db.Column(db.Integer, db.ForeignKey('user_info.user_id'), primary_key=True)
+    year = db.Column(db.Integer, primary_key=True)
+    money_spent = db.Column(db.Float, nullable=False)
+
     def to_dict(self):
         return {
-            'user_id' :self.user_id,
-            'money_spend': self.money_spend,
-            'year': self.year
+            'user_id': self.user_id,
+            'year': self.year,
+            'money_spent': self.money_spent
         }
