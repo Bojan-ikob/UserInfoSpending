0474aa4d5f690911d7ec688e84b94b076295a17e
petko <petkovski.bojan.8891@gmail.com>
2025-01-13 12:56:59 +0100

    dodadeni ruti i funkcinalnosti koi na dadena ruta  prikazuvaat kolku vkupno ima potroseno sekoj korisnik od tabelite kako i prosecna potrosuvacka po zadadeni vozrasni grupi ... dodadena i funkcionalnos koja gi dodava korisnicite koi imaat vkupna potrosuvacka pogolema od 2000 vo MongoDB

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-
 from flask import Flask, request, jsonify, render_template
 from extensions import db
 from models import UserInfo, UserSpending
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
+
+uri = "mongodb+srv://petkovskibojan8891:f0y4QHEggv8ox8cB@cluster0.iiujb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri, server_api=ServerApi('1'))
+mongo_db = client['UsersInfoSpending']
+users_vouchers_collection = mongo_db['users_vouchers']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_vouchers.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -13,16 +19,93 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+@app.route("/")
+def home_page_view():
+    return jsonify("Users spending info")
+
 @app.route('/users', methods=['GET'])
 def get_users():
     user_info = UserInfo.query.all()
     return jsonify([user.to_dict() for user in user_info])
 
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = UserInfo.query.get_or_404(id)
+    return jsonify(user.to_dict())
+
+@app.route('/user_spending', methods = ['POST'])
+def add_spending():
+    data = request.get_json()
+    new_spending = UserSpending(user_id = data['user_id'], money_spent = data['money_spent'], year = data['year'])
+    db.session.add(new_spending)
+    db.session.commit()
+    return jsonify(new_spending.to_dict()),201
 
 @app.route('/user_spending', methods=['GET'])
 def get_user_spending():
     user_spending = UserSpending.query.all()
-    return jsonify([user.to_dict() for user in user_spending])
+    spendings_list = [user.to_dict() for user in user_spending]
+
+    users_vouchers = []
+    users = UserInfo.query.all()
+
+    for user in users:
+        spendings = UserSpending.query.filter_by(user_id=user.user_id).all()
+        total_spent = sum([spending.money_spent for spending in spendings])
+
+        if total_spent > 2000:
+            user_data = {
+                "user_id": user.user_id,
+                "name": user.name,
+                "email": user.email,
+                "age": user.age,
+                "total_spent": total_spent,
+                "spendings": [{"money_spent": spending.money_spent, "year": spending.year} for spending in spendings]
+            }
+            users_vouchers.append(user_data)
+
+    if users_vouchers:
+        users_vouchers_collection.insert_many(users_vouchers)
+
+    return jsonify(spendings_list)
+
+@app.route('/total_spent/<int:user_id>', methods=['GET'])
+def get_total_spent(user_id):
+    spendings = UserSpending.query.filter_by(user_id=user_id).all()
+    total_spent = sum([spending.money_spent for spending in spendings])
+    return jsonify({"user_id": user_id, "total_spent": total_spent})
+
+@app.route('/average_spending_by_age', methods=['GET'])
+def average_spending_by_age():
+    age_groups = {
+        "18-24": (18, 24),
+        "25-30": (25, 30),
+        "31-36": (31, 36),
+        "37-47": (37, 47),
+        ">47": (48, 100)
+    }
+
+    average_spending_by_age = {}
+
+    for group, (min_age, max_age) in age_groups.items():
+        users_in_group = UserInfo.query.filter(UserInfo.age >= min_age, UserInfo.age <= max_age).all()
+        total_spent = 0
+        total_users = 0
+
+        for user in users_in_group:
+            spendings = UserSpending.query.filter_by(user_id=user.user_id).all()
+            total_spent += sum([spending.money_spent for spending in spendings])
+            total_users += 1
+
+        if total_users > 0:
+            average_spending = total_spent / total_users
+            average_spending = round(average_spending, 2)
+        else:
+            average_spending = 0
+
+        average_spending_by_age[group] = average_spending
+
+    return jsonify(average_spending_by_age)
 
 
 
--- models.py
+++ models.py
@@ -1,31 +1,78 @@
-from email.policy import default
-
 from extensions import db
+
+
 
 class UserInfo(db.Model):
     __tablename__ = 'user_info'
-
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    age = db.Column(db.Integer, nullable=False)
+    age = db.Column(db.Integer)
+
+    # Врска со таблицата user_spending
+    spendings = db.relationship('UserSpending', backref='user', lazy=True)
+
     def to_dict(self):
         return {
-            'user_id':self.user_id,
-            'name':self.name,
-            'email':self.email,
-            'age':self.age
+            'user_id': self.user_id,
+            'name': self.name,
+            'email': self.email,
+            'age': self.age,
+            'spendings': [spending.to_dict() for spending in self.spendings]  # Додај ги трошоците
         }
+
 
 class UserSpending(db.Model):
-    __tablename__ = "user_spending"
-
+    __tablename__ = 'user_spending'
     user_id = db.Column(db.Integer, db.ForeignKey('user_info.user_id'), primary_key=True)
-    money_spent = db.Column(db.Float, nullable = False, default = 0.0)
-    year = db.Column(db.Integer, nullable = False)
+    money_spent = db.Column(db.Float)
+    year = db.Column(db.Integer)
+
     def to_dict(self):
         return {
             'user_id':self.user_id,
-            'money_spent':self.money_spent,
-            'year':self.year
+            'money_spent': self.money_spent,
+            'year': self.year
         }
+
+
+
+
+
+# class UserInfo(db.Model):
+#     __tablename__ = 'user_info'
+#
+#     user_id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(100), nullable=False, unique=True)
+#     age = db.Column(db.Integer, nullable=False)
+#
+#
+#     def to_dict(self):
+#         user_spendings = UserSpending.query.filter_by(user_id=self.user_id).all()
+#         spendings_list = [{"money_spent": spending.money_spent, "year": spending.year} for spending in user_spendings]
+#
+#         return {
+#             'user_id':self.user_id,
+#             'name':self.name,
+#             'email':self.email,
+#             'age':self.age,
+#             'spendings': spendings_list
+#
+#         }
+#
+# class UserSpending(db.Model):
+#     __tablename__ = "user_spending"
+#
+#     user_id = db.Column(db.Integer, db.ForeignKey('user_info.user_id'), primary_key = True)
+#     money_spent = db.Column(db.Float, nullable = False, default = 0.0)
+#     year = db.Column(db.Integer, nullable = False)
+#     user = db.relationship('UserInfo', backref=db.backref('spending', lazy=True))
+#     def to_dict(self):
+#         return {
+#             'user_id':self.user_id,
+#             'money_spent':self.money_spent,
+#             'year':self.year
+#
+#         }
+
