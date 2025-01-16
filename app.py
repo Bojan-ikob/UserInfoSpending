3d7e824a17140cb53aa7585fb9147ee7daad8a86
petko <petkovski.bojan.8891@gmail.com>
2025-01-16 10:52:13 +0100

    dodadena funkcionalnost pri dodavanjeto vo mongoDB da proveruva dali podatocite veke se vneseni za korisnicite spored email adresata i podatocite da se vnesuvaat kako na oblak taka i lokalno

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify
-from extensions import db, migrate
+from extensions import db
 from models import UserInfo, UserSpending
-from pymongo import MongoClient
-import os
+from pymongo import MongoClient, errors
 
 app = Flask(__name__)
 
@@ -11,26 +10,51 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_vouchers.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-migrate.init_app(app, db)
 
-# MongoDB server
-# uri = "mongodb+srv://petkovskibojan8891:f0y4QHEggv8ox8cB@cluster0.iiujb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-# client = MongoClient(uri, server_api=ServerApi('1'))
-# mongo_db = client['UsersInfoSpending']
-# users_vouchers_collection = mongo_db['users_vouchers']
-
-# MongoDB lokalno
-client = MongoClient("mongodb://localhost:27017/")
-mongo_db = client["ProjectUserSpending"]
-users_vouchers_collection = mongo_db["user_voucher_collection"]
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 
-# with app.app_context():
-#     db.create_all()
+cloud_uri = "mongodb+srv://petkovskibojan8891:f0y4QHEggv8ox8cB@cluster0.iiujb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+cloud_client = MongoClient(cloud_uri, server_api=ServerApi('1'), serverSelectionTimeoutMS=5000)
+cloud_db = cloud_client["ProjectUserSpending"]
+cloud_collection = cloud_db["user_voucher_collection"]
+
+
+local_uri = "mongodb://localhost:27017/"
+
+
+local_client = MongoClient(local_uri, serverSelectionTimeoutMS=5000)
+local_db = local_client["ProjectUserSpending"]
+local_collection = local_db["user_voucher_collection"]
+
+def add_data_to_both(datapoint):
+    try:
+        if not cloud_collection.find_one({"user_id": datapoint["user_id"]}):
+            cloud_collection.insert_one(datapoint)
+            print("Data added to cloud MongoDB.")
+        else:
+            print(f"Data for user_id {datapoint['user_id']} already exists in cloud MongoDB.")
+    except errors.PyMongoError as e:
+        print(f"Failed to add data to cloud MongoDB: {e}")
+    try:
+        if not local_collection.find_one({"user_id": datapoint["user_id"]}):
+            local_collection.insert_one(datapoint)
+            print("Data added to local MongoDB.")
+        else:
+            print(f"Data for user_id {datapoint['user_id']} already exists in local MongoDB.")
+    except errors.PyMongoError as e:
+        print(f"Failed to add data to local MongoDB: {e}")
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home_page_view():
-    return jsonify("Users spending info")
+    return jsonify("Users spending info home page")
 
 @app.route('/users', methods=['GET'])
 def get_users():
@@ -45,6 +69,9 @@ def get_user(id):
 @app.route('/user_info', methods = ['POST'])
 def add_user():
     data = request.get_json()
+    existing_user = UserInfo.query.filter_by(email=data['email']).first()
+    if existing_user:
+        return jsonify({"message": "User with this email already exists."}), 409
     new_user = UserInfo(name = data['name'], email = data['email'], age = data['age'])
     db.session.add(new_user)
     db.session.commit()
@@ -66,20 +93,29 @@ def get_user_spending():
 
 @app.route('/write_to_mongodb', methods=['POST'])
 def write_to_mongodb():
-    users = UserInfo.query.all()
-    users_vouchers = []
-    for user in users:
-        spendings = UserSpending.query.filter_by(user_id=user.user_id).all()
-        total_spent = sum([spending.money_spent for spending in spendings])
-        if total_spent > 2000:
-            user_data = {
-                "user_id": user.user_id,
-                "total_spent": total_spent,
-            }
-            users_vouchers.append(user_data)
-    if users_vouchers:
-        users_vouchers_collection.insert_many(users_vouchers)
-    return jsonify({"message": "Uspesno vneseni podatoci vo MongoDB!", "count": len(users_vouchers)})
+    try:
+        users = UserInfo.query.all()
+        users_vouchers = []
+        for user in users:
+            spendings = UserSpending.query.filter_by(user_id=user.user_id).all()
+            total_spent = sum([spending.money_spent for spending in spendings])
+            if total_spent > 2000:
+                user_data = {
+                    "user_id": user.user_id,
+                    "total_spent": total_spent,
+                }
+                users_vouchers.append(user_data)
+        if users_vouchers:
+            for data in users_vouchers:
+                add_data_to_both(data)
+            message = f"Vneseni {len(users_vouchers)} podatoci vo MongoDB lokalno i na oblak!"
+            return jsonify({"message": message, "count": len(users_vouchers)}), 201
+        else:
+            return jsonify({"message": "Nema korisnici koi go isponuvaat uslovot za da bidat vneseni vo MongoDB"}), 200
+
+    except Exception as e:
+        return jsonify({"error": "Nastana greska", "details": str(e)}), 500
+
 
 @app.route('/total_spent/<int:user_id>', methods=['GET'])
 def get_total_spent(user_id):
@@ -115,13 +151,5 @@ def average_spending_by_age():
 
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5001)
-os.environ['FLASK_APP'] = 'app.py'
 if __name__ == '__main__':
-    flask_app = os.getenv('FLASK_APP')
-    if flask_app:
-        print(f"FLASK_APP is set to: {flask_app}")
-    else:
-        print("FLASK_APP is not set. Please set it using 'export FLASK_APP=app.py' (Linux/Mac) or 'set FLASK_APP=app.py' (Windows).")
     app.run(debug=True, port=5001)
